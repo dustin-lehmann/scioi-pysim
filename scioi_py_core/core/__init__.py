b250dbff1f0999ad349327d050641e21c9fe47cf
Dustin Lehmann <dustin.lehmann@tu-berlin.de>
2022-09-02 10:56:45 +0200

    update

--- scioi_py_core/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import environment
-from . import spaces
-from . import world
-from . import dynamics
-from . import scheduling
-from . import environment
-from . import physics
-
--- simulation/simulation.py
+++
@@ -1,9 +0,0 @@
-from scioi_py_core import core as core
-
-
-class SimulationEnvironment(core.environment.Environment):
-    pass
-
-
-if __name__ == '__main__':
-    print("HELLO")
